f5d280b264bfa64a425468569818c5f55842b83c
Gustavo Antonio Lastra Colorado <gustavo.lastra@iteratec.com>
2021-05-17 17:57:15 +0200

    add Microservice

--- pricing-service/src/main/resources/data.sql
+++ pricing-service/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO price (id, currency, price, vehicleId) VALUES (1, 'AUD',24524.00,1);
-INSERT INTO price (id, currency, price, vehicleId) VALUES (2, 'USD',352235.00,2);
+INSERT INTO price (id, currency, price) VALUES (1, 'AUD', 24524.00);
+INSERT INTO price (id, currency, price) VALUES (2, 'USD', 352235.00);
