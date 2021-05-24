6e3a1df9e1c4ce2056d5e5657a48273d96305b6e
Gustavo Antonio Lastra Colorado <gustavo.lastra@iteratec.com>
2021-05-24 15:39:18 +0200

    add get Car with address

--- vehicles-api/src/main/resources/data.sql
+++ vehicles-api/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO car (id, condition) VALUES (1, 'USED');
-INSERT INTO car (id, condition) VALUES (2, 'NEW');
+INSERT INTO car (id, condition, lat, lon) VALUES (1, 'USED',20.0,30.0);
+INSERT INTO car (id, condition, lat, lon) VALUES (2, 'NEW',20.0,30.0);
